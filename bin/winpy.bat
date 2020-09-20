d2233e56a674dd6e7109ef2101c6bf248aa8b07b
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-09-20 01:36:05 -0400

    Update winpy

--- bin/winpy.bat
+++ bin/winpy.bat
@@ -1,4 +1,7 @@
 @echo off
+set conda_env=aris
+CALL C:\ProgramData\Anaconda3\condabin\conda.bat activate %conda_env%
+
 set script=%1
 
 set argv=
@@ -9,5 +12,7 @@ set argv=%argv% %1
 shift
 goto loop
 
+
 :main
 wsl alias python=python.exe; . $(wslpath -a "%script%") %argv%
+conda deactivate
