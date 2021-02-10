526cb047f3dc9a351627d3856e0a5aeebb680ff4
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-02-10 01:20:15 -0500

    Fix evaconda.bat

--- bin/evaconda.bat
+++ bin/evaconda.bat
@@ -1,4 +1,4 @@
 @echo off
 
-if not [%1] == [] (set ENV="\envs\")
-C:\Users\garyw\anaconda3\Scripts\activate.bat %USERPROFILE%\anaconda3%ENV%%1
+if not [%1] == [] (set ENV=envs\%1)
+C:\Users\garyw\anaconda3\Scripts\activate.bat %USERPROFILE%\anaconda3\%ENV%
