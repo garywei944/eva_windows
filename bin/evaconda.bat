151125813fc2226f649ced1b48265690a2f7a2e3
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-10-31 21:31:34 -0400

    Update scripts

--- bin/evaconda.bat
+++ bin/evaconda.bat
@@ -1,4 +1,5 @@
 @echo off
 
-if not [%1] == [] (set ENV=envs\%1)
-C:\Users\garyw\anaconda3\Scripts\activate.bat %USERPROFILE%\anaconda3\%ENV%
+rem if not [%1] == [] (set ENV=envs\%1)
+rem C:\Users\garyw\anaconda3\Scripts\activate.bat %USERPROFILE%\anaconda3\%ENV%
+%windir%\system32\cmd.exe "/K" C:\ProgramData\mambaforge\Scripts\activate.bat C:\ProgramData\mambaforge
